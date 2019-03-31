0bbd9dcb417603005e094819860eab006c5baafd
Яков Вишневский <47566864+IakovWish@users.noreply.github.com>
2019-04-01 00:22:31 +0300

    Update main.c

--- lab5/main.c
+++ lab5/main.c
@@ -5,19 +5,19 @@ int main(void)
 {
 	int orig[N]; // массив из N элементов вводится с клавиатуры
 	int copy[N]; // массив в который перезаписывается массив x
-	int i; // позиция символа в исходной строке
-	int a; // позиция символа в результирующей строке
+	int ish; // позиция символа в исходной строке
+	int res; // позиция символа в результирующей строке
 	printf("Please, enter 10 integer numbers\nWARNING: they will be rearranged in reverse order\n");
-	for (i = 0; i < N; i++) // ввод массива x и копирование его в y
+	for (ish = 0; ish < N; ish++) // ввод массива x и копирование его в y
 	{
-		scanf("%d", &orig[i]);
-		copy[i] = orig[i];
+		scanf("%d", &orig[ish]);
+		copy[ish] = orig[ish];
 	}
 	printf("Result:\n");
-	for (i = 0, a = 9; i < N; i++, a--) // переставляем числа в зеркальном порядке и выводим
+	for (ish = 0, res = 9; ish < N; ish++, res--) // переставляем числа в зеркальном порядке и выводим
 	{
-		orig[i] = copy[a];
-		printf("%d ", orig[i]);
+		orig[ish] = copy[res];
+		printf("%d ", orig[ish]);
 	}
 	return 0;
 }
