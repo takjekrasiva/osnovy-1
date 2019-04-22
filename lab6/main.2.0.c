2bf7e81833d1199d11a2443447eda32b112ac0c2
Яков Вишневский <47566864+IakovWish@users.noreply.github.com>
2019-04-22 22:21:02 +0300

    Create main.2.0.c

---
+++ lab6/main.2.0.c
@@ -0,0 +1,71 @@
+#pragma warning(disable : 4996)
+#include <stdio.h>
+
+#define YES 1
+#define NO 0
+#define N 10
+#define K 10
+
+int main(void)
+{
+	int MAS[N][K]; // обьявляем двумерный массив
+	int str; // позиция числа по строке
+	int stlb; // позиция числа по столбцу
+	int obrstlb; // позиция по столбцу с конца
+	int flag; // искомый признак
+
+	srand(time(NULL));
+
+	/*вводим каждый элемент массива*/
+	for (str = 0; str < N; str++)
+	{
+		for (stlb = 0; stlb < K; stlb++)
+		{
+			MAS[str][stlb] = 0 + rand() % 10;
+		}
+	}
+
+	/*вывод исходного массива для подсказки*/
+	printf("original mass\n");
+	for (str = 0; str < N; str++)
+	{
+		for (stlb = 0; stlb < K; stlb++)
+		{
+			printf("%4d ", MAS[str][stlb]);
+		}
+		printf("\n");
+	}
+	printf("\n");
+	
+	/*ищем зеркальные строки*/
+	printf("processed mass\n");
+	for (str = 0; str < N; str++)
+	{
+		flag = YES;
+		for (stlb = 0, obrstlb = K-1; stlb < obrstlb; stlb++, obrstlb--)
+		{
+			if (MAS[str][stlb] != MAS[str][obrstlb])
+			{
+				flag = NO; // помечаем не зеркальные строки
+			}
+		}
+		if (flag == YES) // зеркальные строки аннулируем и выводим
+		{
+			for (stlb = 0; stlb < K; stlb++)
+			{
+				MAS[str][stlb] = 0;
+				printf("%4d ", MAS[str][stlb]);
+			}
+			printf("\n");
+		}
+		else
+		{
+			for (stlb = 0; stlb < K; stlb++) // не зеркальные строки просто выводим
+			{
+				printf("%4d ", MAS[str][stlb]);
+			}
+			printf("\n");
+		}
+	}
+	return 0;
+}
