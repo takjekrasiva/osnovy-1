d02b01289911af6c28d1d88bf6df59dbb9d729e5
Яков Вишневский <47566864+IakovWish@users.noreply.github.com>
2019-05-17 02:35:03 +0300

    Update main.c

--- lab7/main.c
+++ lab7/main.c
@@ -1,2 +1,19 @@
-#pragma warning(disable : 4996) //необходимо для использования устаревших функций (scanf)
-#include <stdio.h>
+#pragma warning(disable : 4996)
+#include <stdio.h> 
+
+int main()
+{
+	unsigned long res = 0; // результат
+	unsigned long mask = 1; // маска
+	unsigned long N; // исходное число
+	scanf("%x", &N);
+	while (N != 0)
+	{
+		res ^= (N & mask); // смотрим последнее число и записываем его в результат
+		N = N >> 1; // меняем последнее число, сдвигая N
+		res = res << 1; // свобождаем место под следующее число в результате
+	}
+	res = res >> 1; // если N = 0 значит надо отменить последнее действие
+	printf("%x", res);
+	return 0;
+}
